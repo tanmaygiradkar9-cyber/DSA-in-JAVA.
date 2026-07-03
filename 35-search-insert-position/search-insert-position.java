141ce3390f6ddb08709329d77a594d032841bc8a
tanmaygiradkar9-cyber <tanmaygiradkar9@gmail.com>
2026-07-03 15:47:56 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (28.55%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int searchInsert(int[] arr, int target) {
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            else if(arr[mid]<target){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+
+        }
+        return start;
+        
+    }
+}
