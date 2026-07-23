49fa9059ae61edec6a6898078605537deb0e3c16
tanmaygiradkar9-cyber <tanmaygiradkar9@gmail.com>
2026-07-23 19:56:42 +0530

    Time: 4 ms (99.94%) | Memory: 46.3 MB (94.96%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -1,25 +1,43 @@
 class Solution {
-    public static  String removeDuplicates(String s) {
-        Stack<Character>st=new Stack<>();
+    // public static  String removeDuplicates(String s) {
+    //     Stack<Character>st=new Stack<>();
         
-        int n=s.length();
-        for(int i=0;i<n;i++){
-            char ch=s.charAt(i);
-            if (!st.empty()&& st.peek()==ch) {
-                st.pop();
+    //     int n=s.length();
+    //     for(int i=0;i<n;i++){
+    //         char ch=s.charAt(i);
+    //         if (!st.empty()&& st.peek()==ch) {
+    //             st.pop();
                
                 
             
-            }
-            else{
-                st.push(ch);
-            }
-        }
-        StringBuilder sb=new StringBuilder();
-        for(char ch: st){
-            sb.append(ch);
-        }
-        return sb.toString();
+    //         }
+    //         else{
+    //             st.push(ch);
+    //         }
+    //     }
+    //     StringBuilder sb=new StringBuilder();
+    //     for(char ch: st){
+    //         sb.append(ch);
+    //     }
+    //     return sb.toString();
         
+    // }
+    
+    public String removeDuplicates(String s) {
+        char[] arr = s.toCharArray();
+        int write = 0;
+
+        for (int read = 0; read < arr.length; read++) {
+            arr[write] = arr[read];
+
+            if (write > 0 && arr[write] == arr[write - 1]) {
+                write--;      // Remove duplicate pair
+            } else {
+                write++;      // Keep character
+            }
+        }
+
+        return new String(arr, 0, write);
     }
+
 }
