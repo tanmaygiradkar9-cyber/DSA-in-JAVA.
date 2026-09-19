2b65daff2f8b1c22018b73fb7363ab3613ec1389
tanmaygiradkar9-cyber <tanmaygiradkar9@gmail.com>
2026-09-19 07:41:01 +0530

    Time: 24 ms (28.36%) | Memory: 81.7 MB (83.22%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,24 @@
+class Solution {
+    public  boolean containsDuplicate(int[] nums){
+        // Arrays.sort(nums);
+        // for(int i=1;i<nums.length;i++){
+        //     if(nums[i]==nums[i-1]){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+
+
+
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1]){
+                return true;
+            }
+             
+        }
+        return false;
+
+    }
+}
